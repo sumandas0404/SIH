471e2772e43c4c7777ce18458335a4a23f383d5d
sumandas0404 <sumondas0404@gmail.com>
2023-09-23 13:07:02 +0530

    footer and navlinks data added

---
+++ src/data/footer-links.js
@@ -0,0 +1,56 @@
+export const FooterLink2 = [
+    {
+      title: "Privacy Policy",
+      links: [
+        { title: "Data Collection And Usage", link: "/" },
+        { title: "Cookies Policy", link: "/" },
+        { title: "Information Security Measures", link: "/" },
+        { title: "Data Sharing Practices", link: "/" },
+        { title: "Children's Online Privacy", link: "/" },
+        { title: "Compliance with Data Protection Laws", link: "/" },
+        { title: "Privacy Impact Assessments", link: "/" },
+        { title: "Data Breach Notification Process", link: "/" },
+        { title: "Developer Tools", link: "/" },
+        { title: "Opt-Out Options and Preferences", link: "/" },
+        { title: "Adhar KYC", link: "/" },
+        { title: "Privacy Policy for Mobile Apps", link: "/" },
+        { title: "Bank Accounts", link: "/" },
+        { title: "Math", link: "/" },
+        { title: "Mobile Development", link: "/" },
+        { title: "Web Design", link: "/" },
+        { title: "International Data Transfers", link: "/" },
+      ],
+    },
+    {
+      title: "About Us",
+      links: [
+        { title: "Mission and Vision", link: "/" },
+        { title: "Leadership Team", link: "/" },
+        { title: "History and Milestones", link: "/" },
+        { title: "Strategic Plan", link: "/" },
+        { title: "Agency Structure", link: "/" },
+        { title: "Goals and Objectives", link: "/" },
+        { title: "Annual Reports", link: "/" },
+        { title: "Statutory Authority", link: "/" },
+        { title: "Regulatory Framework", link: "/" },
+        { title: "Agency Committees and Boards", link: "/" },
+        { title: "Diversity and Inclusion", link: "/" },
+        { title: "Ethics and Code of Conduct", link: "/" },
+        { title: "Employment Opportunities", link: "/" },
+        { title: "Goals and Objectives", link: "/" },
+      ],
+    },
+    {
+      title: " Contact Us",
+      links: [
+          {title: "Office Locations and Hours", link: "/"},
+          {title: "Staff Directory", link: "/"},
+          {title: "Public Feedback Form", link: "/"},
+          {title: "Public Inquiries", link: "/"},
+          {title: "Media Contacts", link: "/"},
+          {title: "Accessibility Assistance", link: "/"},
+          {title: "Social Media Contacts", link: "/"}
+      ]
+    }
+  ];
+  
---
+++ src/data/navbar-links.js
@@ -0,0 +1,16 @@
+export const NavbarLinks = [
+    {
+      title: "Home",
+      path: "/",
+    },
+
+    {
+      title: "Schemes",
+      path: "/about",
+    },
+    {
+      title: "Contact Us",
+      path: "/contact",
+    },
+  ];
+  
