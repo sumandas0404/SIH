3bb738ea6a18d8b4b1672850b1a5731754e87eed
sumandas0404 <sumondas0404@gmail.com>
2023-09-23 13:18:44 +0530

    Home page feature-slider added

---
+++ src/components/core/Slider/sliderImage.js
@@ -0,0 +1,45 @@
+import First from "../../../assets/1.png";
+import Second from "../../../assets/2.png";
+import Third from "../../../assets/3.png";
+import fourth from "../../../assets/4.png";
+import fifth from "../../../assets/5.png";
+import sixth from "../../../assets/6.png";
+import seventh from "../../../assets/7.png";
+
+export default [
+  {
+    title: "FEATURE 1",
+    description: "Describe Feature 1",
+    urls: First,
+  },
+  {
+    title: "FEATURE 2",
+    description: "Describe Feature 2",
+    urls: Second,
+  },
+  {
+    title: "FEATURE 3",
+    description: "Describe Feature 3",
+    urls: Third,
+  },
+  {
+    title: "FEATURE 4",
+    description: "Describe Feature 4",
+    urls: fourth,
+  },
+  {
+    title: "FEATURE 5",
+    description: "Describe Feature 5",
+    urls: fifth,
+  },
+  {
+    title: "FEATURE 6",
+    description: "Describe Feature 6",
+    urls: sixth,
+  },
+  {
+    title: "FEATURE 7",
+    description: "Describe Feature 7",
+    urls: seventh,
+  },
+];
